251f284590cb88e2827fb8b0fbbcefeef76563f3
Sanjay Prajapati <sanjay270899@gmail.com>
2021-02-09 15:16:39 +0530

    Update charts

--- frontend/src/components/layout/Charts.js
+++ frontend/src/components/layout/Charts.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import LineChart from './LineChart';
-import PieChart from './PieChart';
+import '../styles/Charts.css';
+import {Line, Pie} from 'react-chartjs-2';
 
 function Charts() {
 
@@ -8,7 +8,10 @@ function Charts() {
         labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
         datasets: [{
             label: "Number of Hours Code",
-            data: [5, 2, 3, 5, 7, 6, 10]
+            data: [5, 2, 3, 5, 7, 6, 10],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
         }]
     };
 
@@ -16,20 +19,73 @@ function Charts() {
         labels: ["HTML", "CSS", "JS", "Java", "C++"],
         datasets: [{
             label: "Number of Problems",
-            data: [5, 2, 3, 5, 7]
+            data: [5, 2, 3, 5, 7],
+            backgroundColor: randomColorArray(5, 0.3),
+            borderColor: 'white',
+            borderWidth: 1
         }]
     };
 
+    function randomColorArray(size, alpha) {
+        let colors = [];
+        for(let i=0; i<size; i=i+1) {
+            let red = Math.round(Math.random()*255);
+            let green = Math.round(Math.random()*255);
+            let blue = Math.round(Math.random()*255);
+
+            colors.push(`rgba(${red}, ${green}, ${blue}, ${alpha})`);
+        }
+        return colors;
+    }
+
     return (
-        <div className="chart">
-            <LineChart
-                title="Daily Activity"
-                data={dailyActivityData}
-            />
-            <PieChart
-                title="Programming Languages"
-                data={programmingLanguagesData}
-            />
+        <div className="charts">
+            <div className="line-chart">
+                <div className="chart-title">Daily Activity</div>
+                <div className="chart-container">
+                    <Line
+                        data={{
+                            labels: dailyActivityData.labels,
+                            datasets: dailyActivityData.datasets
+                        }}
+                        options={{
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [
+                                    {
+                                        ticks: {
+                                            beginAtZero: true,
+                                        },
+                                    },
+                                ],
+                            }
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="pie-chart">
+                <div className="chart-title">Programming Languages</div>
+                <div className="chart-container">
+                    <Pie
+                        data={{
+                            labels: programmingLanguagesData.labels,
+                            datasets: programmingLanguagesData.datasets
+                        }}
+                        options={{
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [
+                                    {
+                                        ticks: {
+                                            beginAtZero: true,
+                                        },
+                                    },
+                                ],
+                            }
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
